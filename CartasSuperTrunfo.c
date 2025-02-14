df9e5915f6ffc15643fd9416df44edeb80727ffc
Sara <sara.rosa.sr44@gmail.com>
2025-02-14 16:53:27 +0000

    Declarei as variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,200 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    char Código A01 [4];
+    char Nome A01 [20];
+    int População A01;
+    float Pib A01;
+    int Pontos turísticos A01;
+
+    char Código A02 [4];
+    char Nome A02 [20];
+    int População A02;
+    float Pib A02;
+    int Pontos turísticos A02;
+
+    char Código A03 [4];
+    char Nome A03 [20];
+    int População A03;
+    float Pib A03;
+    int Pontos turísticos A03;
+
+    char Código A04 [4];
+    char Nome A04 [20];
+    int População A04;
+    float Pib A04;
+    int Pontos turísticos A04;
+
+    char Código B01 [4];
+    char Nome B01 [20];
+    int População B01;
+    float Pib B01;
+    int Pontos turísticos B01;
+
+    char Código B02 [4];
+    char Nome B02 [20];
+    int População B02;
+    float Pib B02;
+    int Pontos turísticos B02;
+
+    char Código B03 [4];
+    char Nome B03 [20];
+    int População B03;
+    float Pib B03;
+    int Pontos turísticos B03;
+
+    char Código B04 [4];
+    char Nome B04 [20];
+    int População B04;
+    float Pib B04;
+    int Pontos turísticos B04;
+
+    char Código C01 [4];
+    char Nome C01 [20];
+    int População C01;
+    float Pib C01;
+    int Pontos turísticos C01;
+
+    char Código C02 [4];
+    char Nome C02 [20];
+    int População C02;
+    float Pib C02;
+    int Pontos turísticos C02;
+
+    char Código C03 [4];
+    char Nome C03 [20];
+    int População C03;
+    float Pib C03;
+    int Pontos turísticos C03;
+
+    char Código C04 [4];
+    char Nome C04 [20];
+    int População C04;
+    float Pib C04;
+    int Pontos turísticos C04;
+    
+
+    char Código D01 [4];
+    char Nome D01 [20];
+    int População D01;
+    float Pib D01;
+    int Pontos turísticos D01;
+
+    char Código D02 [4];
+    char Nome D02 [20];
+    int População D02;
+    float Pib D02;
+    int Pontos turísticos D02;
+
+    char Código D03 [4];
+    char Nome D03 [20];
+    int População D03;
+    float Pib D03;
+    int Pontos turísticos D03;
+
+    char Código D04 [4];
+    char Nome D04 [20];
+    int População D04;
+    float Pib D04;
+    int Pontos turísticos D04;
+
+    char Código E01 [4];
+    char Nome E01 [20];
+    int População E01;
+    float Pib E01;
+    int Pontos turísticos E01;
+
+    char Código E02 [4];
+    char Nome E02 [20];
+    int População E02;
+    float Pib E02;
+    int Pontos turísticos E02;
+
+    char Código E03 [4];
+    char Nome E03 [20];
+    int População E03;
+    float Pib E03;
+    int Pontos turísticos E03;
+
+    char Código E04 [4];
+    char Nome E04 [20];
+    int População E04;
+    float Pib E04;
+    int Pontos turísticos E04;
+
+    char Código F01 [4];
+    char Nome F01 [20];
+    int População F01;
+    float Pib F01;
+    int Pontos turísticos F01;
+
+    char Código F02 [4];
+    char Nome F02 [20];
+    int População F02;
+    float Pib F02;
+    int Pontos turísticos F02;
+
+    char Código F03 [4];
+    char Nome F03 [20];
+    int População F03;
+    float Pib F03;
+    int Pontos turísticos F03;
+
+    char Código F04 [4];
+    char Nome F04 [20];
+    int População F04;
+    float Pib F04;
+    int Pontos turísticos F04;
+
+    char Código G01 [4];
+    char Nome G01 [20];
+    int População G01;
+    float Pib G01;
+    int Pontos turísticos G01;
+
+    char Código G02 [4];
+    char Nome G02 [20];
+    int População G02;
+    float Pib G02;
+    int Pontos turísticos G02;
+
+
+    char Código G03 [4];
+    char Nome G03 [20];
+    int População G03;
+    float Pib G03;
+    int Pontos turísticos G03;
+    
+    char Código G04 [4];
+    char Nome G04 [20];
+    int População G04;
+    float Pib G04;
+    int Pontos turísticos G04;
+
+    char Código H01 [4];
+    char Nome H01 [20];
+    int População H01;
+    float Pib H01;
+    int Pontos turísticos H01;
+
+    char Código H02 [4];
+    char Nome H02 [20];
+    int População H02;
+    float Pib H02;
+    int Pontos turísticos H02;
+
+    char Código H03 [4];
+    char Nome H03 [20];
+    int População H03;
+    float Pib H03;
+    int Pontos turísticos H03;
+
+    char Código H04 [4];
+    char Nome H04 [20];
+    int População H04;
+    float Pib H04;
+    int Pontos turísticos H04;
+
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
