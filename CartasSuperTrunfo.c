359fef84359dd65fdc675e32b46b140c5e8aa18e
Sara <sara.rosa.sr44@gmail.com>
2025-02-24 12:42:45 +0000

    Print e Scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,213 +4,96 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-    char Codigo1 [4];  //Nesse momento eu estou declarando as variaveis de 8 estados (A a H) e sua respectivas cidades (1 a 4)
+    char Codigo1 [4];  
     char Nome1 [20];
     int Populacao1;
     float Pib1;
     int PontosTuristicos1;
-
-    char Codigo2 [4];
+    char Estado1 [20];
+    
+    char Codigo2 [4];  
     char Nome2 [20];
     int Populacao2;
     float Pib2;
     int PontosTuristicos2;
 
-    char Codigo3 [4];
+    char Codigo3 [4];  
     char Nome3 [20];
     int Populacao3;
     float Pib3;
     int PontosTuristicos3;
-
-    char Codigo4 [4];
+    char Estado2 [20];
+    
+    char Codigo4 [4];  
     char Nome4 [20];
     int Populacao4;
     float Pib4;
     int PontosTuristicos4;
 
-    char Codigo5 [4];
-    char Nome5 [20];
-    int Populacao5;
-    float Pib5;
-    int PontosTuristicos5;
-
-    char Codigo6 [4];
-    char Nom6 [20];
-    int Populacao6;
-    float Pib6;
-    int PontosTuristicos6;
-
-    char Codigo7 [4];
-    char Nome7 [20];
-    int Populacao7;
-    float Pib7;
-    int PontosTuristicos7;
-
-    char Codigo8 [4];
-    char Nome8 [20];
-    int Populacao8;
-    float Pib8;
-    int PontosTuristicos8;
-
-    char Codigo9 [4];
-    char Nome9 [20];
-    int Populacao9;
-    float Pib9;
-    int PontosTuristicos9;
-
-    char Codigo10 [4];
-    char Nome10 [20];
-    int Populacao10;
-    float Pib10;
-    int PontosTuristicos10;
-    
-
-    char Codigo11 [4];
-    char Nome11 [20];
-    int Populacao11;
-    float Pib11;
-    int PontosTuristicos11;
-
-    char Codigo12 [4];
-    char Nome12 [20];
-    int Populacao12;
-    float Pib12;
-    int PontosTuristicos12;
-
-    char Codigo13 [4];
-    char Nome13 [20];
-    int Populacao13;
-    float Pib13;
-    int PontosTuristicos13;
-
-    char Codigo14 [4];
-    char Nome14 [20];
-    int Populacao14;
-    float Pib14;
-    int PontosTuristicos14;
-
-    char Codigo15 [4];
-    char Nome15 [20];
-    int Populacao15;
-    float Pib15;
-    int PontosTuristicos15;
-
-    char Codigo16 [4];
-    char Nome16 [20];
-    int Populacao16;
-    float Pib16;
-    int PontosTuristicos16;
-
-    char Codigo17 [4];
-    char Nome17 [20];
-    int Populacao17;
-    float Pib17;
-    int PontosTuristicos17;
-
-    char Codigo18 [4];
-    char Nome18 [20];
-    int Populacao18;
-    float Pib18;
-    int PontosTuristicos18;
-
-    char Codigo19 [4];
-    char Nome19 [20];
-    int Populacao19;
-    float Pib19;
-    int PontosTuristicos19;
-
-    char Codigo20 [4];
-    char Nome20 [20];
-    int Populacao20;
-    float Pib20;
-    int PontosTuristicos20;
-
-    char Codigo21 [4];
-    char Nome21 [20];
-    int Populacao21;
-    float Pib21;
-    int PontosTuristicos21;
-
-    char Codigo22 [4];
-    char Nome22 [20];
-    int Populacao22;
-    float Pib22;
-    int PontosTuristicos22;
-
-
-    char Codigo23 [4];
-    char Nome23 [20];
-    int Populacao23;
-    float Pib23;
-    int PontosTuristicos23;
-    
-    char Codigo24 [4];
-    char Nome24 [20];
-    int Populacao24;
-    float PibG04;
-    int PontosTuristicos24;
-
-    char Codigo25 [4];
-    char Nome25 [20];
-    int Populacao25;
-    float Pib25;
-    int PontosTuristicos25;
-
-    char Codigo26 [4];
-    char Nome26 [20];
-    int Populacao26;
-    float Pib26;
-    int PontosTuristicos26;
-
-    char Codigo27 [4];
-    char Nome27 [20];
-    int Populacao27;
-    float Pib27;
-    int PontosTuristicos27;
-
-    char Codigo28 [4];
-    char Nome28 [20];
-    int Populacao28;
-    float Pib28;
-    int PontosTuristicos28;
-
-    char Codigo29 [4];
-    char Nome29 [20];
-    int Populacao29;
-    float Pib29;
-    int PontosTuristicos29;
-
-    char Codigo30 [4];
-    char Nome30 [20];
-    int Populacao30;
-    float Pib30;
-    int PontosTuristicos30;
-
-    char Codigo32 [4];
-    char Nome32 [20];
-    int Populacao32;
-    float Pib32;
-    int PontosTuristicos32;
-
 
     
 
-
-    // Cadastro das Cartas:
-   printf("Digite o código da cidade: \n");
+    
+    
+    
+    // Cadastro das Cartas: 
+   printf("Digete o estado das cartas (Ex:A,B,C):");
+   scanf("%s", Estado1);
+   printf("Digite o código da cidade (Ex:A01,A02): \n");
    scanf("%s", Codigo1);
    printf("Digite o nome da cidade: \n");
    scanf("%s", Nome1);
    printf("Digete o número da populaçao: \n");
-   scanf("%d", Populacao1);
-   printf("Digite o Pib: \n")";
+   scanf("%d", &Populacao1);
+   printf("Digite o Pib: \n");
+   scanf("%f", &Pib1);
+   printf("Digite o número de pontos turísticos: \n");
+   scanf("%d", &PontosTuristicos1);
+
+   printf("Digite o código da cidade: \n");
+   scanf("%s", Codigo2);
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", Nome2);
+   printf("Digete o número da populaçao: \n");
+   scanf("%d", &Populacao2);
+   printf("Digite o Pib: \n");
+   scanf("%f", &Pib2);
+   printf("Digite o número de pontos turísticos: \n");
+   scanf("%d", &PontosTuristicos2);
+
+   printf("Digete outro estado:");
+   scanf("%s", Estado2);
+   printf("Digite o código da cidade: \n");
+   scanf("%s", Codigo3);
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", Nome3);
+   printf("Digete o número da populaçao: \n");
+   scanf("%d", &Populacao3);
+   printf("Digite o Pib: \n");
+   scanf("%f", &Pib3);
+   printf("Digite o número de pontos turísticos: \n");
+   scanf("%d", &PontosTuristicos3);
+
+   printf("Digite o código da cidade: \n");
+   scanf("%s", Codigo4);
+   printf("Digite o nome da cidade: \n");
+   scanf("%s", Nome4);
+   printf("Digete o número da populaçao: \n");
+   scanf("%d", &Populacao4);
+   printf("Digite o Pib: \n");
+   scanf("%f", &Pib4);
+   printf("Digite o número de pontos turísticos: \n");
+   scanf("%d", &PontosTuristicos4);
+
+
+
    
+
        
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
